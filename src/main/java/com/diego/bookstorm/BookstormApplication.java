a658b8e7527fb972bdfe875c0c8dbb3ce7da3aad
Diego-de <diego04dgs@gmail.com>
2021-09-01 12:39:45 -0300

    commit inicial

---
+++ src/main/java/com/diego/bookstorm/BookstormApplication.java
@@ -0,0 +1,13 @@
+package com.diego.bookstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/bookstorm/BookstormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.bookstorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
